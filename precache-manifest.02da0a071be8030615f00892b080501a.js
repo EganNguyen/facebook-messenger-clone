2a69a4d99c247b321c395423b6b94737e1551a1a
hoangduy <duynguyen.joy@gmail.com>
2020-09-13 13:42:56 +0700

    Updates

---
+++ precache-manifest.02da0a071be8030615f00892b080501a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1c64f21ddedf496a53d0a24eb2bcbc",
+    "url": "/facebook-messenger-clone/index.html"
+  },
+  {
+    "revision": "fc31ecc6d433b45e1eee",
+    "url": "/facebook-messenger-clone/static/css/main.db130bbe.chunk.css"
+  },
+  {
+    "revision": "b188aeae694efe89284a",
+    "url": "/facebook-messenger-clone/static/js/2.31463e63.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-messenger-clone/static/js/2.31463e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc31ecc6d433b45e1eee",
+    "url": "/facebook-messenger-clone/static/js/main.5b1dad36.chunk.js"
+  },
+  {
+    "revision": "eeedfeb3aa0589659840",
+    "url": "/facebook-messenger-clone/static/js/runtime-main.ffb12647.js"
+  }
+]);
